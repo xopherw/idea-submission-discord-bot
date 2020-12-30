56dc592bdb33db91c35697fb2a399dd62a9905c9
Christopher Wong <31772094+xopherw@users.noreply.github.com>
2020-12-30 16:10:10 +0800

    Update idea_sub_bot.py

--- idea_sub_bot.py
+++ idea_sub_bot.py
@@ -6,7 +6,7 @@ bot = commands.Bot(command_prefix='$')  # Select a prefix like '$', or '!', or '
 
 @bot.command(name='idea') # You can change your command calling name here
 async def isb(ctx, arg1, arg2, arg3 = ''):
-    channel = bot.get_channel('YOUR-DESIRED-CHANNEL-ID-IN-INTEGER')
+    channel = bot.get_channel('YOUR-DESIRED-CHANNEL-ID-IN-INTEGER') # Since discord channel ID are mainly a set of numbers, you do not need quotation mark
     name = ctx.author if(arg3 is '') else arg3
     myEmbed = discord.Embed(title=arg1, description=arg2, color=0x00ff00)
     myEmbed.set_author(name=name)
